421904c329989cc98b14ed0ed0118489f2a8f825
geoff thorpe <geoff.thorpe@ft.com>
2018-01-02 10:30:41 +0000

    First version

---
+++ README.md
@@ -0,0 +1,131 @@
+# Serverless About Plugin
+
+[![serverless](http://public.serverless.com/badges/v3.svg)](http://www.serverless.com)
+[![npm (scoped)](https://img.shields.io/npm/v/@financial-times/serverless-plugin-about.svg)](https://www.npmjs.com/package/@financial-times/serverless-plugin-about)
+[![npm](https://img.shields.io/npm/dw/@financial-times/serverless-plugin-about)](https://www.npmjs.com/package/@financial-times/serverless-plugin-about)
+[![license](https://img.shields.io/npm/l/serverless-plugin-about.svg)](https://raw.githubusercontent.com/Financial-Times/serverless-plugin-about/master/LICENSE)
+
+An about endpount to describe your lambdas.
+
+**Requirements:**
+
+* Serverless _v1.12.x_ or higher.
+* AWS provider
+
+## How it works
+
+About creates a new endpoint (named \_\_about by default) which can be called to provide a json summary of the current purpose of each lambda.
+
+## Setup
+
+Install via npm in the root of your Serverless service:
+
+```
+npm install serverless-plugin-about --save-dev
+```
+
+* Add the plugin to the `plugins` array in your Serverless `serverless.yml`:
+
+```yml
+plugins:
+  - serverless-plugin-about
+```
+
+* Add a `about` property to all the events in all the functions you want to be documented.
+
+```yml
+functions:
+  hello:
+    events
+      - http:
+          path: /schema/{TypeID}
+          method: get
+          private: false
+          about:
+            purpose: Output of the schema for a type of data
+            params: TypeID: the type of data
+      - http:
+          path: /schema/{TypeID}/{FieldID}
+          method: get
+          private: false
+          about:
+            purpose: Output of the schema for a field
+            params: TypeID and FieldID
+```
+
+* Add additional properties to trigger the output of a full description for each lambda
+
+```yml
+         about:
+            purpose: Output of the schema for a type of data
+            params: TypeID: the type of data
+            id: fullschema
+            lastUpdated: []
+```
+
+Note that the ok and lastUpdated are reserved and will automatically be populated, as follows:
+o lastUpdated is the date.time at which the about was ran
+
+* about to be able to `invoke` lambdas requires the following Policy Statement in `iamRoleStatements`:
+
+```yaml
+iamRoleStatements:
+  - Effect: 'Allow'
+    Action:
+      - 'lambda:InvokeFunction'
+    Resource:
+    - Fn::Join:
+      - ':'
+      - - arn:aws:lambda
+        - Ref: AWS::Region
+        - Ref: AWS::AccountId
+        - function:${self:service}-${opt:stage, self:provider.stage}-*
+```
+
+## Options
+
+* **cleanFolder** (default `true`)
+* **memorySize** (default `128`)
+* **name** (default `${service}-${stage}-about-plugin`)
+* **timeout** (default `10` seconds)
+* **endpoint** (default `__about`)
+
+```yml
+custom:
+  about:
+    cleanFolder: false,
+    memorySize: 256
+    name: 'show_me_info'
+    timeout: 20
+    endpoint: _show_info
+```
+
+* define a custom header for the about to give the output some context
+
+```yml
+    endpoint: __about
+    format:
+      schemaVersion: 1
+      name: A great system that uses about
+      systemCode: greatsys
+      lambdas: []
+```
+
+Note that lambdas is reserved and is used to identify the location into which the array of lambda infomation responses will be placed
+
+## Artifact
+
+If you are doing your own [package artifact](https://serverless.com/framework/docs/providers/aws/guide/packaging#artifact) set option `cleanFolder` to `false` and run `serverless package`. This will allow you to extract the `healthcheck` NodeJS lambda file from the `_healthcheck` folder and add it in your custom artifact logic.
+
+## Contribute
+
+Help us making this plugin better and future proof.
+
+* Clone the code
+* Install the dependencies with `npm install`
+* Create a feature branch `git checkout -b new_feature`
+* Lint with standard `npm run lint`
+
+## License
+
+This software is released under the MIT license. See [the license file](LICENSE) for more details.
---
+++ src/index.js
@@ -0,0 +1,165 @@
+'use strict'
+
+const BbPromise = require('bluebird')
+const fs = BbPromise.promisifyAll(require('fs-extra'))
+const path = require('path')
+const tokenSubstitute = require('token-substitute')
+
+class About {
+  constructor (serverless, options) {
+    this.serverless = serverless
+    this.options = options
+    this.custom = this.serverless.service.custom
+    this.provider = this.serverless.getProvider('aws')
+
+    this.hooks = {
+      'after:package:initialize': this.afterPackageInitialize.bind(this),
+      'after:package:createDeploymentArtifacts': this.afterCreateDeploymentArtifacts.bind(this)
+    }
+  }
+
+  afterPackageInitialize () {
+    this.configPlugin()
+    return this.createAbout()
+  }
+
+  afterCreateDeploymentArtifacts () {
+    return this.cleanFolder()
+  }
+
+  configPlugin () {
+    this.folderName = '_about'
+    if (this.custom && this.custom.about && typeof this.custom.about.folderName === 'string') {
+      this.folderName = this.custom.about.folderName
+    }
+    this.pathFolder = this.getPath(this.folderName)
+    this.pathFile = this.pathFolder + '/serverless-plugin-about.js'
+    this.pathHandler = this.folderName + '/serverless-plugin-about.about'
+
+    /** Default options */
+    this.about = {
+      cleanFolder: true,
+      memorySize: 128,
+      name: this.serverless.service.service + '-' + this.options.stage + '-about-plugin',
+      timeout: 10,
+      endpoint: '__about'
+    }
+
+    /** Set global custom options */
+    if (!this.custom || !this.custom.about) {
+      return
+    }
+
+    /** Clean folder */
+    if (typeof this.custom.about.cleanFolder === 'boolean') {
+      this.about.cleanFolder = this.custom.about.cleanFolder
+    }
+
+    /** Memory size */
+    if (typeof this.custom.about.memorySize === 'number') {
+      this.about.memorySize = this.custom.about.memorySize
+    }
+
+    /** Function name */
+    if (typeof this.custom.about.name === 'string') {
+      this.about.name = this.custom.about.name
+    }
+
+    /** Timeout */
+    if (typeof this.custom.about.timeout === 'number') {
+      this.about.timeout = this.custom.about.timeout
+    }
+
+    /** Endpoint name */
+    if (typeof this.custom.about.endpoint === 'string') {
+      this.about.endpoint = this.custom.about.endpoint
+    }
+  }
+
+  getPath (file) {
+    return path.join(this.serverless.config.servicePath, file)
+  }
+
+  cleanFolder () {
+    if (!this.about.cleanFolder) {
+      return Promise.resolve()
+    }
+    return fs.removeAsync(this.pathFolder)
+  }
+
+  createAbout () {
+    return this.getFunctionsWithAbouts()
+      .then((functionNames) => {
+        return this.createAboutFunctionArtifact(functionNames)
+      }).then(() => {
+        return this.addAboutFunctionToService()
+      })
+  }
+
+  getFunctionsWithAbouts () {
+    const allFunctions = this.serverless.service.getAllFunctions().map((functionName) => {
+      const functionObject = this.serverless.service.getFunction(functionName)
+      return {
+        name: functionObject.name,
+        about: this.getEventsWithAbouts(functionName)
+      }
+    })
+
+    return Promise.resolve(allFunctions)
+  }
+
+  getEventsWithAbouts (functionName) {
+    return this.serverless.service.getAllEventsInFunction(functionName)
+      .reduce((abouts, eventObject) => {
+        if (eventObject.http.about) {
+          abouts.push({
+            info: eventObject.http.about
+          })
+        }
+        return abouts
+      }, [])
+  }
+
+  createAboutFunctionArtifact (functionObjects) {
+    const aboutTemplate = fs.readFileSync(path.resolve(__dirname, './about_plugin_template.js'), 'utf8')
+    const aboutFunction = tokenSubstitute(aboutTemplate, {
+      tokens: {
+        creationDate: new Date().toISOString(),
+        components: JSON.stringify(functionObjects),
+        outputHeader: JSON.stringify(this.custom.about.format)
+      }
+    })
+
+    return fs.outputFileAsync(this.pathFile, aboutFunction)
+  }
+
+  addAboutFunctionToService () {
+    this.serverless.service.functions.aboutPlugin = {
+      description: 'Serverless About Plugin',
+      events: [
+        {
+          http: {
+            path: this.about.endpoint,
+            method: 'get',
+            private: false
+          }
+        }
+      ],
+      handler: this.pathHandler,
+      memorySize: this.about.memorySize,
+      name: this.about.name,
+      runtime: 'nodejs6.10',
+      package: {
+        individually: true,
+        exclude: ['**'],
+        include: [this.folderName + '/**']
+      },
+      timeout: this.about.timeout
+    }
+
+    return this.serverless.service.functions.aboutPlugin
+  }
+}
+
+/** Export About class */
+module.exports = About
